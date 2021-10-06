025dda4c6ce96372a04f2d58ce9da11e014b47dc
HJJ <wasicrob27@gmail.com>
2021-10-06 23:25:09 +0900

    블럭에 관련돤 Command Derived Class 추가

--- src/input-component.h
+++ src/input-component.h
@@ -1,17 +1,72 @@
 #ifndef CLI_TETRIS_INPUT_COMPONENT_H_
 #define CLI_TETRIS_INPUT_COMPONENT_H_
 
-namespace cli_tetris {
+#include "object-defined.h"
 
+namespace cli_tetris {
+/* Command Class ----------------------------------------------------------------------------------- */
 class Command {
    public:
     virtual ~Command() {}
     virtual void Excute() = 0;
 };
 
-class MoveObjectCommand : public Command {
-    
-}
+class ChangeBlockCommand : public Command {
+   private:
+    Object* object_;
+
+   public:
+    ChangeBlockCommand(Object* object = nullptr);
+    virtual ~ChangeBlockCommand();
+
+    virtual void Excute() override;
+};
+
+class FallBlockCommand : public Command {
+   private:
+    Object* object_;
+
+   public:
+    FallBlockCommand(Object* Object = nullptr);
+    virtual ~FallBlockCommand();
+
+    virtual void Excute() override;
+};
+
+class MoveDownBlockCommand : public Command {
+   private:
+    Object* object_;
+
+   public:
+    MoveDownBlockCommand(Object* Object = nullptr);
+    virtual ~MoveDownBlockCommand();
+
+    virtual void Excute() override;
+};
+
+class MoveLeftBlockCommand : public Command {
+   private:
+    Object* object_;
+
+   public:
+    MoveLeftBlockCommand(Object* Object = nullptr);
+    virtual ~MoveLeftBlockCommand();
+
+    virtual void Excute() override;
+};
+
+class MoveRightBlockCommand : public Command {
+   private:
+    Object* object_;
+
+   public:
+    MoveRightBlockCommand(Object* Object = nullptr);
+    virtual ~MoveRightBlockCommand();
+
+    virtual void Excute() override;
+};
+
+/* InputHandler Class ----------------------------------------------------------------------------------- */
 
 }  // namespace cli_tetris
 
