e9f4b3dc6bee74f3600fe3ed13d91906fd95dae5
HJJ <wasicrob27@gmail.com>
2021-11-13 17:05:19 +0900

    timer-handler, global변수 timer_id 저장방법 변경, queue -> 순회

--- src/timer-handler.cc
+++ src/timer-handler.cc
@@ -22,22 +22,34 @@ static constexpr unsigned int kNIF = -1;  // Nerver In Field
 // Timer 개수
 static constexpr unsigned int kTIMER_NUM = 10;
 
-//원형 queue와 같은 방식이며 이름만 다르다.
-static unsigned int timer_result_start_id = 0;    //원형 queue의 head
-static unsigned int timer_result_current_id = 0;  //원형 queue의 tail
+// timer 수신 결과 여부 확인
+static bool timer_result_list[kTIMER_NUM] = {
+    false,
+};
 
-static bool timer_result_list[kTIMER_NUM];  // timer queue 이면서 변경 여부 확인.
-static bool timer_usage[kTIMER_NUM];        // timer queue 이면서 변경 여부 확인.
+// timer 사용 여부 확인.
+static bool timer_usage[kTIMER_NUM] = {
+    false,
+};
 
 static void AlarmHandler(int signal_num, siginfo_t* signal_info, void* ucontext) {
     // while ()
 }
 
 static bool IsFullTimerQueue() {
-    if ((timer_result_current_id + 1 % kTIMER_NUM) == timer_result_start_id)
-        return true;
-    else
-        return false;
+    for (unsigned int i = 0; i <= kTIMER_NUM; ++i) {
+        if (!timer_usage[i]) return false;
+    }
+    return true;
+}
+
+static int GetTimerID() {
+    for (unsigned int i = 0; i <= kTIMER_NUM; ++i) {
+        if (!timer_usage[i]) {
+            timer_usage[i] = true;
+            return i;
+        }
+    }
 }
 
 /* TimerAccessor Class ===================================================================================== */
@@ -77,6 +89,12 @@ TimerHandler::TimerHandler() {
     sigac.sa_mask = set;
     sigac.sa_sigaction = timer::AlarmHandler;
     sigaction(SIGALRM, &sigac, NULL);
+
+    // Global variable init again
+    for (int i = 0; i < kTIMER_NUM; ++i) {
+        timer_result_list[i] = false;
+        timer_usage[i] = false;
+    }
 }
 
 TimerHandler::~TimerHandler() {
@@ -87,22 +105,22 @@ TimerAccessor&& TimerHandler::CreateTimer() {
     if (timer::IsFullTimerQueue()) throw std::runtime_error(std::string("E006 : Timer Full"));
 
     // key 생성
-    TimerAccessor key(timer::timer_result_current_id, &timer::timer_result_list[timer_result_current_id]);
+    int id = timer::GetTimerID();
+    TimerAccessor key(id, &timer::timer_result_list[id]);
     TimerData data(std::make_shared<bool>(true));
     key.is_allive_ = data.accessor_allive_;
 
     // Linux System call for timer_create
     int ret = 0;
     struct sigevent evp;
-    evp.sigev_value.sival_int = timer::timer_result_current_id;
+    evp.sigev_value.sival_int = id;
     evp.sigev_notify = SIGEV_SIGNAL;
     evp.sigev_signo = SIGALRM;
     ret = timer_create(CLOCK_REALTIME, &evp, &data.timer_);  // evp가 NULL이면 SIGARM , Value는 Timer id
     if (ret) throw std::runtime_error(std::string("E007 : System call : timer_create error"));
 
     // key 등록
-    timer_list_.insert(std::make_pair(timer::timer_result_current_id, data));
-    timer::timer_result_current_id++;
+    timer_list_.insert(std::make_pair(id, data));
 
     return std::move(key);
 }
