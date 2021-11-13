e77f541d50167a50e43d5cc6999c268306812536
HJJ <wasicrob27@gmail.com>
2021-11-13 15:51:55 +0900

    TimerHandler 초안 작성중..., timer에 대한 key값을 생성 반환 하는 형식으로 이용

---
+++ src/timer-handler.cc
@@ -0,0 +1,109 @@
+#include "timer-handler.h"
+
+#include <exception>
+#include <memory>
+#include <string>
+#include <cassert>
+#include <utility>
+
+#define _POSIX_C_SOURCE 199309L
+#include <signal.h>
+#include <time.h>
+extern "C" {
+}
+
+namespace cli_tetris::timer {
+
+// enum name
+static constexpr unsigned int kNIF = -1;  // Nerver In Field
+
+/* inside setting variable & method ===================================================================================== */
+
+// Timer 개수
+static constexpr unsigned int kTIMER_NUM = 10;
+
+//원형 queue와 같은 방식이며 이름만 다르다.
+static unsigned int timer_result_start_id = 0;    //원형 queue의 head
+static unsigned int timer_result_current_id = 0;  //원형 queue의 tail
+
+static bool timer_result_list[kTIMER_NUM];  // timer queue 이면서 변경 여부 확인.
+
+static void AlarmHandler(int signal_num, siginfo_t* signal_info, void* ucontext) {
+    // while ()
+}
+
+static bool IsFullTimerQueue() {
+    if ((timer_result_current_id + 1 % kTIMER_NUM) == timer_result_start_id)
+        return true;
+    else
+        return false;
+}
+/* class TimerData  ===================================================================================== */
+TimerData::TimerData(std::unique_ptr<bool> accessor_allive) : accessor_allive_(std::move(accessor_allive)) {}
+TimerData::TimerData(TimerData& obj) : accessor_allive_(std::move(obj.accessor_allive_)), timer_(obj.timer_) {}
+TimerData::TimerData(TimerData&& obj) : accessor_allive_(std::move(obj.accessor_allive_)), timer_(obj.timer_) {}
+
+/* TimerAccessor Class ===================================================================================== */
+
+TimerAccessor::TimerAccessor(int id, bool* is_running_address) : id_(id), is_running_(is_running_address), is_allive_(nullptr) {
+    // is_allive 는 TimerHandler::CreateTimer에 의해 생성
+}
+TimerAccessor::~TimerAccessor() {}
+TimerAccessor::TimerAccessor(const TimerAccessor& obj) : id_(obj.id_), is_running_(obj.is_running_), is_allive_(obj.is_allive_) {}  //복사 생성자
+TimerAccessor::TimerAccessor(TimerAccessor&& obj) : id_(obj.id_), is_running_(obj.is_running_), is_allive_(obj.is_allive_) {}
+
+/* TimerHandler Class ===================================================================================== */
+bool TimerHandler::is_initialize_ = false;
+
+TimerHandler::TimerHandler() {
+    assert(is_initialize_);
+    is_initialize_ = true;
+
+    // ALRM SIGNAL Capture
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGALRM);
+    struct sigaction sigac;
+    sigac.sa_flags = SA_SIGINFO;
+    sigac.sa_mask = set;
+    sigac.sa_sigaction = timer::AlarmHandler;
+
+    sigaction(SIGALRM, &sigac, NULL);
+}
+
+TimerHandler::~TimerHandler() {
+    is_initialize_ = false;
+}
+
+TimerData& TimerHandler::FindTimerData(TimerAccessor& accessor) {
+    const auto& itr = timer_list_.find(accessor.id_);
+}
+
+TimerAccessor&& TimerHandler::CreateTimer() {
+    if (timer::IsFullTimerQueue()) throw std::runtime_error(std::string("E006 : Timer Full"));
+
+    // key 생성
+    TimerAccessor key(timer::timer_result_current_id, &timer::timer_result_list[timer_result_current_id]);
+    TimerData data(std::make_unique<bool>(true));
+    key.is_allive_ = data.accessor_allive_.get();
+
+    // Linux System call for timer_create
+    int ret = 0;
+    struct sigevent evp;
+    evp.sigev_value.sival_int = timer::timer_result_current_id;
+    evp.sigev_notify = SIGEV_SIGNAL;
+    evp.sigev_signo = SIGALRM;
+    ret = timer_create(CLOCK_REALTIME, &evp, &data.timer_);  // evp가 NULL이면 SIGARM , Value는 Timer id
+    if (ret) throw std::runtime_error(std::string("E007 : System call : timer_create error"));
+
+    // key 등록
+    timer_list_.insert(std::make_pair(timer::timer_result_current_id, data));
+    timer::timer_result_current_id++;
+
+    return std::move(key);
+}
+
+void TimerHandler::DeleteTimer(TimerAccessor& accessor) {
+}
+
+}  // namespace cli_tetris::timer
---
+++ src/timer-handler.h
@@ -0,0 +1,63 @@
+#ifndef CLI_TETRIS_TIMER_HANDLER_H_
+#define CLI_TETRIS_TIMER_HANDLER_H_
+
+#include <map>
+#include <ctime>
+
+namespace cli_tetris::timer {
+/**
+ * TimerHandler를 위한 key value로 사용됩니다.
+ * TimerHandler의 CreateTimer method를 통해 생성됩니다.
+ */
+class TimerAccessor {
+    friend class TimerHandler;
+
+   private:
+    const unsigned int id_;   // Handler로부터 할당받은 id
+    bool* const is_running_;  // Timer 동작 여부
+    bool* is_allive_;         // Accessor의 수명 확인
+
+   private:
+    TimerAccessor(int id, bool* is_running_address);
+    ~TimerAccessor();
+
+   public:
+    TimerAccessor(const TimerAccessor& obj);  //복사 생성자
+    TimerAccessor(TimerAccessor&& obj) noexcept;
+    TimerAccessor& operator=(const TimerAccessor& obj) = delete;  //일반 대입 생성자는 존재할 수 없다, 이 객체는 유일해야 하므로, 키를 복제할 수 있지만, 양도하거나 변경할 수 없다.
+    TimerAccessor& operator=(TimerAccessor&& obj) = delete;       //대입 생성자는 존재할 수 없다.
+
+    bool IsRunning() const;
+    bool IsAlive() const;
+};
+
+class TimerData {
+   public:
+    std::unique_ptr<bool> accessor_allive_;
+    timer_t timer_;
+
+    TimerData(std::unique_ptr<bool> accessor_allive);
+    TimerData(TimerData& obj);
+    TimerData(TimerData&& obj) noexcept;
+    TimerData& operator=(const TimerData& obj) = delete;
+};
+
+class TimerHandler {
+   private:
+    static bool is_initialize_;            // 생성은 단 한개만 허용
+    std::map<int, TimerData> timer_list_;  // TimderData의 id, 그에 해당하는 timer id
+
+   private:
+    TimerData& FindTimerData(TimerAccessor& accessor);
+
+   public:
+    TimerHandler();
+    ~TimerHandler();
+    TimerAccessor&& CreateTimer();
+    void DeleteTimer(TimerAccessor& accessor);
+    void SetTimer(int sec, int nanosec);
+};
+
+}  // namespace cli_tetris::timer
+
+#endif  // CLI_TETRIS_CUSTOM_TIMER_H_
