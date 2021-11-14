4c55166089c20453f026f9cea7ca73fac75fbf50
HJJ <wasicrob27@gmail.com>
2021-11-14 22:57:54 +0900

    input-layer 추가, 하지만 사용하지 않기로 결정, 자세한 내용이 있음
    Command Pattern을 위해 만들었던 layer이지만 개발 도중 이 패턴이 필요
    없다고 판단되어짐, 그래서 사용하지 않기로 결정했고 그에 따라서
    GameState에 추상 method인 InputProcess도 사용하지 않기로 결정

--- src/input-layer.h
+++ src/input-layer.h
@@ -1,9 +1,33 @@
+/**
+ *  Command Pattern을 위해 생성되었지만, 개발하다보니 현재 Tetris에는
+ *  Command Pattern이 필요없다. 구지 이용하려고 하면 더 번거러워질 우려가 있다.
+ *  사용하지 않는 것으로 한다.
+ */
+
 #ifndef CLI_TETRIS_INPUT_COMPONENT_H_
 #define CLI_TETRIS_INPUT_COMPONENT_H_
 
 #include "object-defined.h"
 
+#include <queue>
+
 namespace cli_tetris {
+class CommandQueue {
+   private:
+    std::queue<Command*> queue_;
+    const int limit_queue_num_;
+
+   private:
+    bool IsFullQueue() const;
+
+   public:
+    CommandQueue(const int& limit_queue);
+    ~CommandQueue();
+    void ResetQueue();
+    bool Put(Command* command);
+    Command* Get();
+};
+
 /* Command Class ----------------------------------------------------------------------------------- */
 class Command {
    public:
@@ -11,6 +35,23 @@ class Command {
     virtual void Excute() = 0;
 };
 
+class MenuItemMoveCommand : public Command {
+   public:
+    using Move = enum Move { kUP = 0,
+                             kDown,
+                             kLeft,
+                             kRight };
+
+   private:
+    Move where_;
+
+   public:
+    MenuItemMoveCommand(Move where);
+    ~MenuItemMoveCommand();
+    void Excute() override;
+};
+
+/* Block key ----------------------------------------------------------------------------------- */
 class ChangeBlockCommand : public Command {
    private:
     Object* object_;
@@ -70,4 +111,4 @@ class MoveRightBlockCommand : public Command {
 
 }  // namespace cli_tetris
 
-#endif  //CLI_TETRIS_INPUT_COMPONENT_H_
+#endif  // CLI_TETRIS_INPUT_COMPONENT_H_
