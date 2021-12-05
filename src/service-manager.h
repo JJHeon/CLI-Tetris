f70f1ff074de5f84ecf273c22a4f758519b38133
HJJ <wasicrob27@gmail.com>
2021-12-05 15:23:47 +0900

    service-manager File Note 추가

--- src/service-manager.h
+++ src/service-manager.h
@@ -1,3 +1,14 @@
+/** Note
+ * service-manager
+ *
+ * Service Mediator Class입니다.
+ * -- 설명 --
+ * UI, Sound, Log, Random 기능을 위한 Service Mediator Pattern
+ * UI를 참조해야할 곳이 GameState 9개 정도 되는데, 추후에 각기 다른 UI를 등록할 수 있도록 합니다.
+ *
+ * -- 변경 이력 -- (21.12.05 이전 기록 없음)
+ */
+
 #ifndef CLI_TETRIS_SERVICE_MANAGER_H_
 #define CLI_TETRIS_SERVICE_MANAGER_H_
 
@@ -5,11 +16,6 @@
 #include "timer-handler.h"
 #include "random-generate-handler.h"
 
-/** UI, Sound, Log, Random 기능을 위한 Service Mediator Pattern
- *  UI를 참조해야할 곳이 GameState 9개 정도 되는데, 추후에 각기 다른 UI를 등록할 수 있도록 하고자 한다.
- *  Service Mediator Pattern을 이용해서 UI를 각 GameState에 전달한다.
- *  주로 StateState의 Initialize 단계에서 각 Ui를 등록하게 한다.
- */
 namespace cli_tetris {
 class Locator final {
     // ui
