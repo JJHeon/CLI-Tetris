b580216d3e051aedd52882c8c1748a7bf9274115
HJJ <wasicrob27@gmail.com>
2022-01-07 09:21:38 +0900

    Add Block Color

--- src/ui-handler.cc
+++ src/ui-handler.cc
@@ -47,12 +47,12 @@ void UiHandler::End() {
 void UiHandler::SetColors() {
     start_color();  // Start color
     init_pair(1, COLOR_BLACK, COLOR_GREEN);
-    init_pair(2, COLOR_BLACK, COLOR_GREEN);
-    init_pair(3, COLOR_BLACK, COLOR_GREEN);
-    init_pair(4, COLOR_BLACK, COLOR_GREEN);
-    init_pair(5, COLOR_BLACK, COLOR_GREEN);
-    init_pair(6, COLOR_BLACK, COLOR_GREEN);
-    init_pair(7, COLOR_BLACK, COLOR_GREEN);
+    init_pair(2, COLOR_BLACK, COLOR_RED);
+    init_pair(3, COLOR_BLACK, COLOR_YELLOW);
+    init_pair(4, COLOR_BLACK, COLOR_BLUE);
+    init_pair(5, COLOR_BLACK, COLOR_MAGENTA);
+    init_pair(6, COLOR_BLACK, COLOR_CYAN);
+    init_pair(7, COLOR_BLACK, COLOR_WHITE);
 
     init_pair(15, COLOR_WHITE, COLOR_BLACK);
 }
